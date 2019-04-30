31f80e63c192168b1b24d88962ea53ec7705605e
zhurof <41473707+zhurof@users.noreply.github.com>
2019-04-30 21:11:11 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,135 @@
+<? include 'header.php';?>	
+<main>
+	<div class="top-block">
+		<div class="wrapper">
+			<div class="top-block__text">
+				<h1 class="top-block__title">Магистр - 15 лет помощи студентам!</h1>
+				<p>Опытные эксперты готовы прийти на помощь</p>
+				<p><a href="#"><strong>Оставьте заявку</strong></a>, и мы свяжемся с Вами <br>в течение <em class="icon-clock"></em> 7 минут</p>
+				<a href="#" class="btn top-block__btn">Заказать работу</a>
+			</div>
+		</div>
+	</div>
+	<div class="services">
+		<div class="wrapper">
+			<h2 class="services__title">Наши услуги - ваши возможности</h2>
+			<div class="services__list">
+				<div class="service-card">
+					<div class="service-card__pic">
+						<img src="img/icons/desktop.svg" alt="" />
+					</div>
+					<div class="service-card__text">
+						<strong class="service-card__emphasis">15 лет</strong>
+						<span>Успешно решаем проблемы с учебой</span>
+					</div>
+				</div>
+				<div class="service-card">
+					<div class="service-card__pic">
+						<img src="img/icons/lecture.svg" alt="" />
+					</div>
+					<div class="service-card__text">
+						<strong class="service-card__emphasis">100 000</strong>
+						<span>Подготовленных студентов</span>
+					</div>
+				</div>
+				<div class="service-card">
+					<div class="service-card__pic">
+						<img src="img/icons/book.svg" alt="" />
+					</div>
+					<div class="service-card__text">
+						<strong class="service-card__emphasis">более 500</strong>
+						<span>Различных учебных дисциплин</span>
+					</div>
+				</div>
+				<div class="service-card">
+					<div class="service-card__pic">
+						<img src="img/icons/student.svg" alt="" />
+					</div>
+					<div class="service-card__text">
+						<strong class="service-card__emphasis">100%</strong>
+						<span>Персональный менеджер, гарантия качества</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="scheme">
+		<div class="wrapper">
+			<div class="scheme__text">
+				<h2 class="scheme__title">Путь студента <br>к душевному спокойствию</h2>
+				<div class="scheme__list">
+					<div class="step">
+						<div class="step__pic"><img src="img/icons/writing.svg" alt=""></div>
+						<div class="step__text">Оформляете заявку </div>
+					</div>
+					<div class="step">
+						<div class="step__pic"><img src="img/icons/chat.svg" alt=""></div>
+						<div class="step__text">Ваш менеджер определяет стоимость услуги.<br> Долго думает какую скидку Вам предоставить </div>
+					</div>
+					<div class="step">
+						<div class="step__pic"><img src="img/icons/calculation.svg" alt=""></div>
+						<div class="step__text">К назначеному сроку, <br>Вы подготовлены и успешно сдадите!</div>
+					</div>
+				</div>
+				<div class="btn scheme__btn">Заказать работу</div>
+			</div>
+		</div>
+	</div>
+	<div class="advantages">
+		<div class="wrapper">
+			<div class="advantages__text">
+				<h2 class="advantages__title">Блин, а кого же выбрать?</h2>
+				<ol class="advantages__list">
+					<li>“Магистр” обзвонил всех конкурентов, и у него самые выгодные цены</li>
+					<li>“Магистр” осуществляет проверку работ в системе “Антиплагиат ВУЗ”. <br>Заметьте, у большинства компаний нет доступа к системе</li>
+					<li>“Магистр” - титан рынка, 15 <br>лет труда, чтобы студенты <br>спали спокойно</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+	<div class="reviews">
+		<div class="wrapper">
+			<h2 class="reviews__title">Счастливые клиенты передают Вам привет</h2>
+			<div class="reviews__slider">
+				<div class="review-card">
+					<img src="img/icons/man.svg" alt="" class="review-card__icon" />
+					<h3 class="review-card__title">Владимир</h3>
+					<div class="review-card__text">Спасибо огромное, всё быстро, а самое главное в срок.</div>
+					<div class="review-card__date">23.04.19</div>
+				</div>
+				<div class="review-card">
+					<img src="img/icons/woman.svg" alt="" class="review-card__icon" />
+					<h3 class="review-card__title">Анастасия</h3>
+					<div class="review-card__text">Спасибо!!! Вы как всегда на высоте.</div>
+					<div class="review-card__date">22.04.19</div>
+				</div>
+				<div class="review-card">
+					<img src="img/icons/man.svg" alt="" class="review-card__icon" />
+					<h3 class="review-card__title">Иван</h3>
+					<div class="review-card__text">Как всегда отличная работа, своевременно, точно и быстро</div>
+					<div class="review-card__date">21.04.19</div>
+				</div>
+				<div class="review-card">
+					<img src="img/icons/man.svg" alt="" class="review-card__icon" />
+					<h3 class="review-card__title">Владимир</h3>
+					<div class="review-card__text">Спасибо огромное, всё быстро, а самое главное в срок.</div>
+					<div class="review-card__date">23.04.19</div>
+				</div>
+				<div class="review-card">
+					<img src="img/icons/woman.svg" alt="" class="review-card__icon" />
+					<h3 class="review-card__title">Анастасия</h3>
+					<div class="review-card__text">Спасибо!!! Вы как всегда на высоте.</div>
+					<div class="review-card__date">22.04.19</div>
+				</div>
+				<div class="review-card">
+					<img src="img/icons/man.svg" alt="" class="review-card__icon" />
+					<h3 class="review-card__title">Иван</h3>
+					<div class="review-card__text">Как всегда отличная работа, своевременно, точно и быстро</div>
+					<div class="review-card__date">21.04.19</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</main>
+<? include 'footer.php';?>
---
+++ js/script.js
@@ -0,0 +1,52 @@
+﻿//Модальные окна
+function openModal(modalId, initiator){  
+  var scrollWidth = window.innerWidth - document.body.clientWidth;//Ширина полосы прокрутки
+  
+	$('.modal-wrapper').children().unwrap();
+	if(!$('#'+modalId).length){
+		alert('Ошибка вызова модального окна');
+		return false;
+	}
+	$('#'+modalId).trigger('beforeShow',initiator).wrap('<div class="modal-wrapper" style="display:none" />');
+	$('.modal-wrapper').fadeIn(400,function(){
+    $('#'+modalId).trigger('afterShow',initiator);
+  });
+	
+	if(scrollWidth){
+		$('html').css('padding-right',scrollWidth);
+		$('body').css('overflow-y','hidden');
+		$('.header').css('width',window.innerWidth  - scrollWidth);
+	}
+}
+function closeModal(){
+	$('.modal-wrapper').fadeOut(200, function(){
+		$('html').css('padding-right','');
+		$('body').css('overflow-y','');
+    $('.header').css('width','');
+	});
+}
+$(document).on('click', '[data-modal]', function(e){
+	e.preventDefault();
+	var modal = $(this).data('modal');
+	openModal(modal,e.target);
+})
+$(document).on('click', '.modal__close', closeModal);
+
+$(document).on('mousedown', '.modal-wrapper', function(e){
+	if(!$('.modal').is(e.target) && $('.modal').has(e.target).length === 0){
+		closeModal();
+	}
+})
+$(document).keydown(function(e){
+	//Закрытие окна на Esc
+	if(e.which == 27){
+		closeModal();
+	}
+});
+
+//отзывы
+$('.reviews__slider').slick({
+	slidesToShow: 3,
+	prevArrow: '<span class="reviews__arrow slick-prev"></span>',
+	nextArrow: '<span class="reviews__arrow slick-next"></span>'
+})
